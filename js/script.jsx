14c8ba30ea31290f9026b3be2036c7a24f193304
Sheng Wu <shengwu34@gmail.com>
2017-04-14 17:08:56 -0700

    mostly working

--- js/script.jsx
+++ js/script.jsx
@@ -1,10 +1,28 @@
+function getRandomElement(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
 class SuggestionGenerator extends React.Component {
+  constructor() {
+    super();
+    this.state = {suggestion: this.generateNewSuggestion()};
+    // Really, ES6?
+    this.setNewSuggestion = this.setNewSuggestion.bind(this);
+  }
+  setNewSuggestion() {
+    this.setState({suggestion: this.generateNewSuggestion()});
+  }
+  generateNewSuggestion() {
+    var adjective = getRandomElement(WORDS['adjectives']);
+    var noun = getRandomElement(WORDS['nouns']);
+    return adjective + ' ' + noun;
+  }
   render() {
     return (
       <div className="suggestion-box">
         <h2>Improv suggestion generator</h2>
-        <div className="suggestion">THIS IS A SUGGESTION</div>
-        <button className="btn btn-primary adjective-verb">GENERATE</button>
+        <div className="suggestion">{ this.state.suggestion }</div>
+        <button className="btn btn-primary adjective-verb" onClick={this.setNewSuggestion}>GENERATE</button>
         <div className="options">
           <button className="btn adjective-verb">Adjective-Noun</button>
           <div className="btn-group" data-toggle="buttons">
