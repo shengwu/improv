59feec862531b410269b573ae14621b1bd52b7ee
Sheng Wu <shengwu34@gmail.com>
2018-06-12 20:17:05 -0700

    Add emotion, animal, job options

--- js/script.jsx
+++ js/script.jsx
@@ -3,7 +3,7 @@ function randomElement(arr) {
 }
 
 function randomElementFromEither(arr1, arr2) {
-  var n = Math.floor(Math.random() * (arr1.length + arr2.length));
+  const n = Math.floor(Math.random() * (arr1.length + arr2.length));
   if (n >= arr1.length) {
     return arr2[n - arr1.length];
   }
@@ -17,6 +17,8 @@ class SuggestionGenerator extends React.Component {
     // Really, ES6?
     this.setSingleWordSuggestion = this.setSingleWordSuggestion.bind(this);
     this.setAdjectiveAndNounSuggestion = this.setAdjectiveAndNounSuggestion.bind(this);
+    this.setEmotionSuggestion = this.setEmotionSuggestion.bind(this);
+    this.setEmotionAndAnimalJobSuggestion = this.setEmotionAndAnimalJobSuggestion.bind(this);
   }
 
   setSingleWordSuggestion() {
@@ -25,13 +27,28 @@ class SuggestionGenerator extends React.Component {
   setAdjectiveAndNounSuggestion() {
     this.setState({suggestion: this.generateAdjectiveAndNounSuggestion()});
   }
+  setEmotionSuggestion() {
+    this.setState({suggestion: this.generateEmotionSuggestion()});
+  }
+  setEmotionAndAnimalJobSuggestion() {
+    this.setState({suggestion: this.generateEmotionAndAnimalJobSuggestion()});
+  }
+
   generateSingleWordSuggestion() {
     return randomElementFromEither(WORDS['adjectives'], WORDS['nouns']);
   }
   generateAdjectiveAndNounSuggestion() {
-    var adjective = randomElement(WORDS['adjectives']);
-    var noun = randomElement(WORDS['nouns']);
+    const adjective = randomElement(WORDS['adjectives']);
+    const noun = randomElement(WORDS['nouns']);
     return adjective + ' ' + noun;
+  }
+  generateEmotionSuggestion() {
+    return randomElement(WORDS['emotions']);
+  }
+  generateEmotionAndAnimalJobSuggestion() {
+    const emotion = randomElement(WORDS['emotions']);
+    const animalOrJob = randomElementFromEither(WORDS['animals'], WORDS['jobs']);
+    return emotion + ' ' + animalOrJob;
   }
 
   render() {
@@ -39,7 +56,9 @@ class SuggestionGenerator extends React.Component {
       <div className="suggestion-box">
         <div className="suggestion">{ this.state.suggestion }</div>
         <button className="btn btn-primary" onClick={ this.setSingleWordSuggestion }>SINGLE WORD</button>
-        <button className="btn btn-info" onClick={ this.setAdjectiveAndNounSuggestion }>ADJECTIVE + NOUN</button>
+        <button className="btn btn-warning" onClick={ this.setAdjectiveAndNounSuggestion }>ADJECTIVE + NOUN</button>
+        <button className="btn btn-info" onClick={ this.setEmotionSuggestion }>EMOTION</button>
+        <button className="btn btn-success" onClick={ this.setEmotionAndAnimalJobSuggestion }>EMOTION + ANIMAL/JOB</button>
       </div>
     );
   }
